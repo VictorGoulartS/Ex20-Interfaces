d3eb4e3b49d10b79f0c7c61109694b04f332935e
Victor <Victor@DESKTOP-SKOFSMV>
2022-08-02 08:02:46 -0300

    Add project files.

---
+++ Exercicio20/Services/PaypalService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Exercicio20.Services;
+
+namespace Exercicio20.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        public double Interest(double amount, int months)
+        {
+            return amount * months * 0.01;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * 0.02;
+        }
+    }
+}
